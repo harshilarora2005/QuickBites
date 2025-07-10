998d9a239a73c8bb1084f30863674a13d77c35b5
harshilarora2005 <harshil.arora205@gmail.com>
2025-07-11 00:24:16 +0530

    fixed all, now pushing final

--- public/common/constants.js
+++ public/common/constants.js
@@ -1,4 +1,4 @@
-export const SWIGGY_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.626547&lng=77.092428&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+export const SWIGGY_API_URL = `https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.626547&lng=77.092428&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
 export const MENU_API_URL = `https://proxy.corsfix.com/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9046136&lng=77.614948&restaurantId=`;
 
